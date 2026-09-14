3930d45ff1b2fc3e1208a3b1ff8145a374ab80df
khanjandave <khanjancentennial@gmail.com>
2026-09-13 23:57:07 -0400

    add federation config for account mfe

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,30 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  APP_INITIALIZER,
+  isDevMode,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideStore } from '@ngrx/store';
+import { provideStore, Store } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
-import { isDevMode } from '@angular/core';
 
 import { authReducer } from './store/auth/auth.reducer';
 import { AuthEffects } from './store/auth/auth.effects';
 import { authInterceptor } from './core/interceptors/auth.interceptors';
+import { AuthActions } from './store/auth/auth.actions';
+
+function initializeAuth(store: Store) {
+  return () => {
+    const token = localStorage.getItem('auth_token');
+    if (token) {
+      store.dispatch(AuthActions.restoreSession({ token }));
+    }
+  };
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,5 +34,11 @@ export const appConfig: ApplicationConfig = {
     provideStore({ auth: authReducer }),
     provideEffects([AuthEffects]),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeAuth,
+      deps: [Store],
+      multi: true,
+    },
   ],
 };
